511d9092ed8489237adc4447b9e26de06fb6b755
Wellington L <wellington-158@hotmail.com>
2021-12-31 18:25:15 -0300

    working to send an email

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
-
+import { BullModule } from '@nestjs/bull';
 import { CreateUserController } from './create-user/create-user.controller';
+import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+
+const {
+  REDIS_HOST,
+  REDIS_PORT,
+  EMAIL_HOST,
+  EMAIL_PORT,
+  EMAIL_USER,
+  EMAIL_PASS,
+} = process.env;
 
 @Module({
-  imports: [], //external
+  imports: [
+    ConfigModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: REDIS_HOST,
+        port: Number(REDIS_PORT),
+      },
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: EMAIL_HOST,
+        port: Number(EMAIL_PORT),
+        auth: {
+          user: EMAIL_USER,
+          pass: EMAIL_PASS,
+        },
+      },
+    }),
+  ], //external
   controllers: [CreateUserController],
   providers: [], //services
 })
--- src/create-user/create-user.controller.ts
+++ src/create-user/create-user.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ICreateUserDto } from './create-user-dto';
 
 @Controller('create-user') //route name
 export class CreateUserController {
-  @Get('/')
-  creteUser() {
-    return 'ok';
+  constructor(private readonly mailService: MailerService) {}
+
+  @Post('/')
+  async createUser(@Body() createUser: ICreateUserDto) {
+    await this.mailService.sendMail({
+      to: createUser.email,
+      from: 'Wellington Test',
+      subject: 'Welcome',
+      text: `Hello ${createUser.name}! Your account has been created`,
+    });
+
+    return { message: createUser };
   }
 }
